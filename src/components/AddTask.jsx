466e81696f6b4cc2034c5f661addcc65caf15c73
Marcos <marcos1jose@live.com>
2023-06-26 01:03:14 -0300

    State - props - tarefas

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./AddTask.css";
+
+const AddTask = () => {
+  return(
+  <div className='add-tesk-container'>
+    <input className='add-tesk-input' type="text" />
+  </div>
+  )
+};
+ 
+export default AddTask;
