b536a4c45e4f310048970e4b953199b3fd646e1d
Marcos <marcos1jose@live.com>
2023-06-28 21:43:19 -0300

    concluir tarefa - deletar tarefa

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Task from './Task';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, handleTaskClick, handleTaskDeletion }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task task={task} key={task.id} />
+        <Task task={task} key={task.id} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
       ))}
     </>
   );
